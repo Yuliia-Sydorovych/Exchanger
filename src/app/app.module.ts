316135c7840bfa4d37f3bfd2770d983233ef5a0e
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2022-06-23 00:11:36 +0300

    Initialize project; Add new favicon; Create two components: header and conventer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConventerComponent } from './conventer/conventer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConventerComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
